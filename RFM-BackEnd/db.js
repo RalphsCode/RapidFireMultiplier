a13f67835cbc6b555a2e6eda54f5958cb99138eb
RalphsCode <RalphsCode@gmail.com>
2025-01-21 11:25:39 -0800

    Updating config files to enable render on Render.com.

--- RFM-BackEnd/db.js
+++ RFM-BackEnd/db.js
@@ -7,9 +7,9 @@ const { Client } = require("pg");
 let DB_URI;
 
 if (process.env.NODE_ENV === "test") {
-  DB_URI = "postgresql://postgres:2024@localhost/rapidfiremultiplier";
+  DB_URI = "postgresql://postgres.umfzfgwofyclooimlqpn:x6ESeLocNtfWSlxE@aws-0-us-west-1.pooler.supabase.com:5432/postgres";
 } else {
-  DB_URI = "postgresql://postgres:2024@localhost/rapidfiremultiplier";
+  DB_URI = "postgresql://postgres.umfzfgwofyclooimlqpn:x6ESeLocNtfWSlxE@aws-0-us-west-1.pooler.supabase.com:5432/postgres";
 }
 
 let db = new Client({
